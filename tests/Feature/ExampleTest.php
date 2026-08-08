435798c101b994bafaaab39b9b4377c470d735f0
Jake Kevin Klair L. Cuenca <jlcuenca.student@asiancollege.edu.ph>
2026-08-08 14:36:37 +0800

    Cover Legal Assistance Division contact information on landing page

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -19,6 +19,13 @@ class ExampleTest extends TestCase
             ->assertSee('Frequently asked questions')
             ->assertSee('No online self-application.')
             ->assertSee('Clearance approval and generation through DAR-LTCMS does not itself execute the legal transfer of land ownership')
+            ->assertSee('DAR Negros Oriental Legal Assistance Division')
+            ->assertSee('522-7144')
+            ->assertSee('0916-876-3071')
+            ->assertSee('dar_legal_orneg@yahoo.com')
+            ->assertSee('(032) 253-6498')
+            ->assertSee('https://www.facebook.com/DARLegalNegor', false)
+            ->assertSee('https://www.dar.gov.ph/home', false)
             ->assertSee(route('login'), false);
     }
 }
