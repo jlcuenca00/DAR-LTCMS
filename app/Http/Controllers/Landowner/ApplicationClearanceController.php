af02e75ce6f54f4e51e362d3ee688dc788b5bca4
Jake Kevin Klair L. Cuenca <jlcuenca.student@asiancollege.edu.ph>
2026-08-08 22:14:18 +0800

    Stream landowner clearance in browser PDF viewer

--- app/Http/Controllers/Landowner/ApplicationClearanceController.php
+++ app/Http/Controllers/Landowner/ApplicationClearanceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Landowner;
 use App\Http\Controllers\Controller;
 use App\Models\Landowner;
 use App\Models\LandTransferApplication;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Support\Facades\Auth;
 
 class ApplicationClearanceController extends Controller
@@ -38,8 +39,28 @@ class ApplicationClearanceController extends Controller
     public function pdf(LandTransferApplication $application)
     {
         $this->authorizeLandownerApplication($application);
+        $application->load(['clearance', 'documents.requiredDocument', 'applicationParcels.parcel']);
 
-        return redirect()->route('landowner.applications.clearance.show', $application);
+        if (! $application->isFinalized()) {
+            return redirect()
+                ->route('landowner.applications.index')
+                ->with('error', 'Decision output is only available after the application is finalized.');
+        }
+
+        if (! $application->clearance) {
+            return redirect()
+                ->route('landowner.applications.index')
+                ->with('error', 'Decision output record is not yet available for this application.');
+        }
+
+        $safeApplicationCode = str_replace(['/', '\\', ' '], '-', (string) $application->application_code);
+
+        $pdf = Pdf::loadView('staff.clearances.pdf', [
+            'application' => $application,
+            'clearance' => $application->clearance,
+        ])->setPaper('a4');
+
+        return $pdf->stream('LTC-Form-No-5-' . $safeApplicationCode . '.pdf');
     }
 
     private function authorizeLandownerApplication(LandTransferApplication $application): void
