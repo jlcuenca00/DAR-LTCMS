2844a3ba3dbb5116e619484f00929aa8a20c1d7e
Jake Kevin Klair L. Cuenca <jlcuenca.student@asiancollege.edu.ph>
2026-08-08 22:30:18 +0800

    Restore landowner LTC Form 5 print view

--- app/Http/Controllers/Landowner/ApplicationClearanceController.php
+++ app/Http/Controllers/Landowner/ApplicationClearanceController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Landowner;
 use App\Http\Controllers\Controller;
 use App\Models\Landowner;
 use App\Models\LandTransferApplication;
-use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Support\Facades\Auth;
 
 class ApplicationClearanceController extends Controller
@@ -13,26 +12,7 @@ class ApplicationClearanceController extends Controller
     public function show(LandTransferApplication $application)
     {
         $this->authorizeLandownerApplication($application);
-        $application->load(['clearance']);
 
-        if (! $application->isFinalized()) {
-            return redirect()
-                ->route('landowner.applications.index')
-                ->with('error', 'Decision output is only available after the application is finalized.');
-        }
-
-        if (! $application->clearance) {
-            return redirect()
-                ->route('landowner.applications.index')
-                ->with('error', 'Decision output record is not yet available for this application.');
-        }
-
-        return redirect()->route('landowner.applications.clearance.pdf', $application);
-    }
-
-    public function pdf(LandTransferApplication $application)
-    {
-        $this->authorizeLandownerApplication($application);
         $application->load(['clearance', 'documents.requiredDocument', 'applicationParcels.parcel']);
 
         if (! $application->isFinalized()) {
@@ -47,38 +27,19 @@ class ApplicationClearanceController extends Controller
                 ->with('error', 'Decision output record is not yet available for this application.');
         }
 
-        $safeApplicationCode = str_replace(['/', '\\', ' '], '-', (string) $application->application_code);
-
-        $html = $this->renderClearancePdfHtml($application);
-        $pdf = Pdf::loadHTML($html)->setPaper('a4');
-
-        return $pdf->stream('LTC-Form-No-5-' . $safeApplicationCode . '.pdf');
-    }
-
-    private function renderClearancePdfHtml(LandTransferApplication $application): string
-    {
-        $html = view('staff.clearances.pdf', [
+        return view('staff.clearances.show', [
             'application' => $application,
             'clearance' => $application->clearance,
-        ])->render();
+            'returnRoute' => route('landowner.applications.index'),
+            'returnLabel' => 'Back to My Applications',
+        ]);
+    }
 
-        $bagongSvgPath = public_path('images/bagong-pilipinas-logo.svg');
+    public function pdf(LandTransferApplication $application)
+    {
+        $this->authorizeLandownerApplication($application);
 
-        if (is_file($bagongSvgPath)) {
-            $svgContents = file_get_contents($bagongSvgPath);
-
-            if ($svgContents !== false) {
-                $svgDataUri = 'data:image/svg+xml;base64,' . base64_encode($svgContents);
-                $html = preg_replace(
-                    '/data:image\/png;base64,[A-Za-z0-9+\/=]+/',
-                    $svgDataUri,
-                    $html,
-                    1
-                ) ?? $html;
-            }
-        }
-
-        return $html;
+        return redirect()->route('landowner.applications.clearance.show', $application);
     }
 
     private function authorizeLandownerApplication(LandTransferApplication $application): void
