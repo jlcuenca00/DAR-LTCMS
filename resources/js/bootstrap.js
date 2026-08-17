54cfabd300767bb70074ec566169352c9ab532a5
Jake Kevin Klair L. Cuenca <jlcuenca.student@asiancollege.edu.ph>
2026-08-17 22:50:59 +0800

    Load single parcel tooltip guard

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import './onboarding-tour';
 import './onboarding-replay-confirmation';
 import './application-intake-flow';
+import './parcel-map-single-tooltip';
 import '../css/staff-dashboard-hero.css';
 
 window.axios = axios;
