e83077dfe68524fcf7bafcf0ae16108a28cdd86d
Jake Kevin Klair L. Cuenca <jlcuenca.student@asiancollege.edu.ph>
2026-08-18 00:22:00 +0800

    load shared staff list filter ui

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,10 +3,12 @@ import './onboarding-tour';
 import './onboarding-replay-confirmation';
 import './application-intake-flow';
 import './application-page-cleanup';
+import './staff-list-filters';
 import './parcel-map-single-tooltip';
 import './development-notice';
 import '../css/staff-dashboard-hero.css';
 import '../css/application-page-cleanup.css';
+import '../css/staff-list-filters.css';
 import '../css/development-notice.css';
 
 window.axios = axios;
