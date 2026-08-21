c8942ddc8d84418a6bd0b9e4d9facc6b7e953ea6
Jake Kevin Klair L. Cuenca <jlcuenca.student@asiancollege.edu.ph>
2026-08-22 04:20:54 +0800

    Register protected source route through routing configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,15 +1,23 @@
 <?php
 
+use App\Http\Controllers\Staff\ProtectedStorageController;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function (): void {
+            Route::middleware(['web', 'auth', 'role:staff'])
+                ->get('/staff/protected-storage/{path}', ProtectedStorageController::class)
+                ->where('path', '.*')
+                ->name('staff.protected-storage.show');
+        },
     )
     ->withMiddleware(function (Middleware $middleware) {
         $trustedProxies = (array) config('app.trusted_proxies', []);
