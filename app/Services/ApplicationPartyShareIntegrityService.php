6623a4a7d8ab0a59aed8d80d7960c134c171f163
Jake Kevin Klair L. Cuenca <jlcuenca.student@asiancollege.edu.ph>
2026-08-22 02:11:48 +0800

    Clean share integrity service import

--- app/Services/ApplicationPartyShareIntegrityService.php
+++ app/Services/ApplicationPartyShareIntegrityService.php
@@ -4,7 +4,6 @@ namespace App\Services;
 
 use App\Models\ApplicationParcel;
 use App\Models\LandTransferApplication;
-use Illuminate\Support\Collection;
 use Illuminate\Validation\ValidationException;
 
 class ApplicationPartyShareIntegrityService
